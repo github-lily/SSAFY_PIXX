b457567c95170f1c65360e6bc5abafbfd1eb785a
rim.ssafy <rim.ssafy@gmail.com>
2025-05-19 15:20:54 +0900

    Feat: people feed에 헤더 안 뜨도록 수정

--- FRONTEND/src/app/(with-menubar)/layout.tsx
+++ FRONTEND/src/app/(with-menubar)/layout.tsx
@@ -7,7 +7,7 @@ import Menubar from "@/components/Menubar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-  const isDetailPage = /^\/(feed|album)\/[^/]+$/.test(pathname); // feed 또는 album 디테일 페이지
+  const isDetailPage = /^\/(feed|album|people)\/[^/]+$/.test(pathname); // feed 또는 album 디테일 페이지
 
   return (
     <div>
